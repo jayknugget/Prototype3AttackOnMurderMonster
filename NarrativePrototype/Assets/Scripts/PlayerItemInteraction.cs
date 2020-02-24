e47a605873780464b35dc53a32a5a6df57c0cc2f
oswald01 <49961157+oswald01@users.noreply.github.com>
2020-02-24 09:09:12 -0600

    player

--- NarrativePrototype/Assets/Scripts/PlayerItemInteraction.cs
+++ NarrativePrototype/Assets/Scripts/PlayerItemInteraction.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class PlayerItemInteraction : MonoBehaviour
 {
     private int keys = 0;
+    private bool firstDoor = true;
+    public GameObject monsterSit;
+
+    private List<Transform> openDoors = new List<Transform>();
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -19,7 +23,7 @@ public class PlayerItemInteraction : MonoBehaviour
         {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit, 3f))
+            if (Physics.Raycast(ray, out hit, 2f))
             {
                 if (hit.transform.CompareTag("Key"))
                 {
@@ -27,9 +31,41 @@ public class PlayerItemInteraction : MonoBehaviour
                     // play sound
                     hit.transform.gameObject.SetActive(false);
                 }
+                else if (hit.transform.CompareTag("Door"))
+                {
+                    bool opened = false;
+                    for (int i = 0; i < openDoors.Count; i++)
+                    {
+                        if (hit.transform == openDoors[i])
+                        {
+                            if (hit.transform.parent.parent.GetComponent<Animator>().GetBool("open"))
+                            {
+                                hit.transform.parent.parent.GetComponent<Animator>().SetBool("open", false);
+                            }
+                            else
+                            {
+                                hit.transform.parent.parent.GetComponent<Animator>().SetBool("open", true);
+                            }
+                            opened = true;
+                            break;
+                         }
+                    }
+                    if (!opened)
+                    {
+                        if (keys >= 1)
+                        {
+                            if (firstDoor)
+                            {
+                                firstDoor = false;
+                                monsterSit.SetActive(false);
+                            }
+                            keys--;
+                            hit.transform.parent.parent.GetComponent<Animator>().SetBool("open", true);
+                            openDoors.Add(hit.transform);
+                        }
+                    }
+                }
             }
         }
-
-
     }
 }
